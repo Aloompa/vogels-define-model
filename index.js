ef0c788c8a852a193d67472bdba20ac24abf728a
Tyson Cadenhead <tysoncadenhead@gmail.com>
2018-07-03 12:15:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+import * as vogels from 'vogels-promisified';
+
+const defineModel = (name, definition) => {
+    const Model = vogels.define(name, definition);
+
+    return Object.assign({}, Model, {
+
+        get: body => 
+            new Promise((resolve, reject) => {
+                Model.get(body, (err, data) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(data);
+                });
+            }),
+
+        query: body => 
+            new Promise((resolve, reject) => {
+                Model.query(body, (err, data) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(data);
+                });
+            }),
+
+        update: body => 
+            new Promise((resolve, reject) => {
+                Model.update(body, (err, data) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(data);
+                });
+            }),
+
+        destroy: body => 
+            new Promise((resolve, reject) => {
+                Model.destroy(body, (err, data) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(data);
+                });
+            }),
+
+        create: body => 
+            new Promise((resolve, reject) => {
+                Model.create(body, (err, data) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(data);
+                });
+            })
+    });
+};
+
+export default defineModel;
